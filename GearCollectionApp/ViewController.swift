c2502dbb14e5911818a13b8732049a9d17dc1e73
daikisakamoto <daiki19910717@gmail.com>
2023-02-08 20:34:52 +0900

    Initial Commit

---
+++ GearCollectionApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GearCollectionApp
+//
+//  Created by 坂本　大貴 on 2023/02/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ GearCollectionAppUITests/GearCollectionAppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  GearCollectionAppUITestsLaunchTests.swift
+//  GearCollectionAppUITests
+//
+//  Created by 坂本　大貴 on 2023/02/08.
+//
+
+import XCTest
+
+final class GearCollectionAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
