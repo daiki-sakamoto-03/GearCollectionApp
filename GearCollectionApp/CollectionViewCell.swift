be391483f3f72ca72b6b9992be1125aa086bd95c
daikisakamoto <daiki19910717@gmail.com>
2023-02-11 23:13:48 +0900

    collectionView実装

---
+++ GearCollectionApp/CollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  CollectionViewCell.swift
+//  GearCollectionApp
+//
+//  Created by 坂本　大貴 on 2023/02/11.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var label: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        backgroundColor = .blue
+    }
+
+}
--- GearCollectionApp/ViewController.swift
+++ GearCollectionApp/ViewController.swift
@@ -8,12 +8,46 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    // カテゴリ分け
+    enum GearType: CaseIterable {
+        case tentAndTarp
+        case tableAndChair
+        case fire
+        case kitchenAndTablewear
+        case sleeping
+        case other
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // セルを登録する
+        collectionView.register(UINib(nibName: "CollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "CustomCell")
     }
-
-
+    
 }
 
+
+// セルを表示させる
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    // セルの数
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return GearType.allCases.count
+    }
+    
+    
+    // セルの設定を行う為のメソッド
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CustomCell", for: indexPath) as! CollectionViewCell
+        
+        return cell
+    }
+}
