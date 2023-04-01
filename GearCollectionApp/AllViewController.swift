f56c2d979ca989e92b1655d11388235cdd73024e
daikisakamoto <daiki19910717@gmail.com>
2023-04-01 23:09:57 +0900

    カテゴリ毎の集計(個数・金額・重量)

--- GearCollectionApp/AllViewController.swift
+++ GearCollectionApp/AllViewController.swift
@@ -23,7 +23,7 @@ class AllViewController: UIViewController, IndicatorInfoProvider {
     let gearDetailVC = GearDetailViewController()
     var record = GearRecord()
     var formatter = DateFormatter()
-
+    
     
     
     override func viewDidLoad() {
@@ -40,7 +40,7 @@ class AllViewController: UIViewController, IndicatorInfoProvider {
         weightLabel.layer.cornerRadius = 10
         weightLabel.clipsToBounds = true
         weightLabel.layer.borderWidth = 1.0
-
+        
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -59,7 +59,7 @@ class AllViewController: UIViewController, IndicatorInfoProvider {
     }
     
     
-// MARK: gearType.gearTypeNameと一致するものだけフィルタリングしてgearDataListに代入する
+    // gearType.gearTypeNameと一致するものだけフィルタリングしてgearDataListに代入する
     func setGearData() {
         let realm = try! Realm()
         let result = realm.objects(GearRecord.self)
@@ -70,10 +70,10 @@ class AllViewController: UIViewController, IndicatorInfoProvider {
                 gearDataList in gearDataList.category == self.gearType.gearTypeName
             }
             gearDataList = Array(filterGearDataList)
-
+            
         }
         
-        }
+    }
     
     // Realmに保存されたデータを合計し、ギアの個数、総額、総重量を表示するメソッド
     func totalIndicate() {
@@ -82,95 +82,104 @@ class AllViewController: UIViewController, IndicatorInfoProvider {
         } catch let error {
             print("エラー")
         }
-        
-        let objects = realm.objects(GearRecord.self)
-        let amountSum = objects.sum(ofProperty: "amount")as Int
-        let weightSum = objects.sum(ofProperty: "weight")as Double
-        numberLabel.text = String(gearDataList.count)
-        amountLabel.text = String(amountSum)
-        weightLabel.text = String(weightSum)
+        // 全ての合計をnumberLabel,amountLabel,weightLabelに表示する場合
+        if self.gearType.gearTypeName == "ALL" {
+            let objects = realm.objects(GearRecord.self)
+            let amountSum = objects.sum(ofProperty: "amount")as Int
+            let weightSum = objects.sum(ofProperty: "weight")as Double
+            numberLabel.text = String(gearDataList.count)
+            amountLabel.text = String(amountSum)
+            weightLabel.text = String(weightSum)
+        } else {
+            // カテゴリ毎の合計をnumberLabel,amountLabel,weightLabelに表示する場合
+            let objects = realm.objects(GearRecord.self)
+            let filterGearDataList = objects.filter("category == %@", self.gearType.gearTypeName)
+            let amountSum = filterGearDataList.sum(ofProperty: "amount")as Int
+            let weightSum = filterGearDataList.sum(ofProperty: "weight")as Double
+            numberLabel.text = String(gearDataList.count)
+            amountLabel.text = String(amountSum)
+            weightLabel.text = String(weightSum)
+        }
     }
-    
-    
-    
 }
     
-
-// 登録したギアを表示するUITableViewのクラス
-extension AllViewController: UITableViewDataSource, UITableViewDelegate {
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return gearDataList.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = allTableView.dequeueReusableCell(withIdentifier: "customCell", for: indexPath) as! AllTableViewCell
-        let gearRecord: GearRecord = gearDataList[indexPath.row]
-        // 各UILabelに、Realmに保存された内容を代入する
-        cell.makerLabel.text = gearRecord.maker
-        cell.nameLabel.text = gearRecord.name
-        cell.amountLabel.text = "\(gearRecord.amount)円"
-        cell.weightLabel.text = "\(gearRecord.weight)kg"
-        cell.dateLabel.text = dateToString.string(from: gearRecord.date)
-        cell.img.image = loadImage(fileName: gearRecord.imageURL)
-        return cell
-    }
-    // 削除機能追加
-    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        let targetGear = gearDataList[indexPath.row]
-        let realm = try! Realm()
-        try! realm.write {
-            realm.delete(targetGear)
+    // 登録したギアを表示するUITableViewのクラス
+    extension AllViewController: UITableViewDataSource, UITableViewDelegate {
+        
+        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return gearDataList.count
         }
-        gearDataList.remove(at: indexPath.row)
-        tableView.deleteRows(at: [indexPath], with: .automatic)
-    }
-    
-    
-    func loadImage(fileName: String) -> UIImage? {
-        guard gearDataList.isEmpty == false else {
-            return nil
+        
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            let cell = allTableView.dequeueReusableCell(withIdentifier: "customCell", for: indexPath) as! AllTableViewCell
+            let gearRecord: GearRecord = gearDataList[indexPath.row]
+            // 各UILabelに、Realmに保存された内容を代入する
+            cell.makerLabel.text = gearRecord.maker
+            cell.nameLabel.text = gearRecord.name
+            cell.amountLabel.text = "\(gearRecord.amount)円"
+            cell.weightLabel.text = "\(gearRecord.weight)kg"
+            cell.dateLabel.text = dateToString.string(from: gearRecord.date)
+            cell.img.image = loadImage(fileName: gearRecord.imageURL)
+            return cell
         }
-        guard fileName.isEmpty == false else {
-            return nil
+        // 削除機能追加
+        func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+            let targetGear = gearDataList[indexPath.row]
+            let realm = try! Realm()
+            try! realm.write {
+                realm.delete(targetGear)
+            }
+            gearDataList.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .automatic)
         }
-        let path = docURL("\(fileName).jpg")!.path
-        if FileManager.default.fileExists(atPath: path) { // fileExists→ディレクトリがどうかチェックするメソッド(引数atPathで示した定数pathが存在する場合はtrue、存在しない場合はfalseを返す)
-            if let image = UIImage(contentsOfFile: path) {
-                return image
-            } else {
-                print("読み込みに失敗しました")
+        
+        
+        func loadImage(fileName: String) -> UIImage? {
+            guard gearDataList.isEmpty == false else {
                 return nil
             }
-        } else {
-            print("画像が見つかりませんでした")
-            return nil
+            guard fileName.isEmpty == false else {
+                return nil
+            }
+            let path = docURL("\(fileName).jpg")!.path
+            if FileManager.default.fileExists(atPath: path) { // fileExists→ディレクトリがどうかチェックするメソッド(引数atPathで示した定数pathが存在する場合はtrue、存在しない場合はfalseを返す)
+                if let image = UIImage(contentsOfFile: path) {
+                    return image
+                } else {
+                    print("読み込みに失敗しました")
+                    return nil
+                }
+            } else {
+                print("画像が見つかりませんでした")
+                return nil
+            }
         }
-    }
-    
-    private func docURL(_ fileName: String) -> URL? {
-        do {
-            let docsURL = try FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
-            
-            let url = docsURL.appendingPathComponent(fileName)
-            return url
-        } catch {
-            return nil
+        
+        private func docURL(_ fileName: String) -> URL? {
+            do {
+                let docsURL = try FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: false)
+                
+                let url = docsURL.appendingPathComponent(fileName)
+                return url
+            } catch {
+                return nil
+            }
         }
+        
+        var dateToString: DateFormatter {
+            let formatter = DateFormatter()
+            formatter.calendar = Calendar(identifier: .gregorian)
+            formatter.locale = Locale(identifier: "ja_jp")
+            formatter.timeZone = TimeZone(identifier: "Asia/Tokyo")
+            formatter.timeStyle = .none
+            formatter.dateFormat = "yyyy年MM月dd日"
+            return formatter
+        }
+        
+        
+        
+        
     }
     
-    var dateToString: DateFormatter {
-        let formatter = DateFormatter()
-        formatter.calendar = Calendar(identifier: .gregorian)
-        formatter.locale = Locale(identifier: "ja_jp")
-        formatter.timeZone = TimeZone(identifier: "Asia/Tokyo")
-        formatter.timeStyle = .none
-        formatter.dateFormat = "yyyy年MM月dd日"
-        return formatter
-    }
-    
-    
-
-    
-}
 
